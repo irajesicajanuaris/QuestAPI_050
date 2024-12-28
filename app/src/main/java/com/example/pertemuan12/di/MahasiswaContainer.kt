f5fbd369b9ba82fd5447c9e8419ef1ae24cc76b6
irajesicajanuaris <jjiraaa1@gmail.com>
2024-12-28 22:43:20 +0700

    menambahkan MahasiswaContainer

---
+++ app/src/main/java/com/example/pertemuan12/di/MahasiswaContainer.kt
@@ -0,0 +1,28 @@
+package com.example.pertemuan12.di
+
+import com.example.pertemuan12.repository.MahasiswaRepository
+import com.example.pertemuan12.service_api.MahasiswaService
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import retrofit2.Retrofit
+
+interface AppContainer{
+    val kontakRepository: MahasiswaRepository
+}
+
+class MahasiswaContainer : AppContainer{
+    private val baseUrl ="http://10.0.2.2:8000/umyTI/" //localhost diganti ip kalau run di hp
+    private val json = Json{ignoreUnknownKeys = true}
+    private val retrofit: Retrofit = Retrofit.Builder()
+        .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
+        .baseUrl(baseUrl).build()
+
+    private val mahasiswaService: MahasiswaService by lazy {
+        retrofit.create(MahasiswaService::class.java) }
+
+    override val kontakRepository: MahasiswaRepository by lazy {
+        MahasiswaService.NetworkKontakRepository(mahasiswaService)
+    }
+
+}
