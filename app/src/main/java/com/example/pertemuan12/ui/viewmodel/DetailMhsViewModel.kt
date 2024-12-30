e2df8bfb4b484d5eaf24bd00b7189b1d6b474147
irajesicajanuaris <jjiraaa1@gmail.com>
2024-12-30 10:10:03 +0700

    menambahkan data class DetailUiState

---
+++ app/src/main/java/com/example/pertemuan12/ui/viewmodel/DetailMhsViewModel.kt
@@ -0,0 +1,27 @@
+
+import com.example.pertemuan12.model.Mahasiswa
+import com.example.pertemuan12.ui.viewmodel.InsertUiEvent
+
+
+
+
+data class DetailUiState(
+    val detailUiEvent: InsertUiEvent = InsertUiEvent(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String = "",
+) {
+    val isUiEventNotEmpty: Boolean
+        get() = detailUiEvent != InsertUiEvent()
+}
+
+fun Mahasiswa.toDetailUiEvent(): InsertUiEvent {
+    return InsertUiEvent(
+        nim = nim,
+        nama = nama,
+        alamat = alamat,
+        jenisKelamin = jenisKelamin,
+        kelas = kelas,
+        angkatan = angkatan
+    )
+}
