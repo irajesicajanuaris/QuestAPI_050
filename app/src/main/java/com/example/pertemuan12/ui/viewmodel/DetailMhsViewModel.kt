ecd7c29953f3da5b9d7107dc01208d99f653f763
irajesicajanuaris <jjiraaa1@gmail.com>
2024-12-30 10:12:35 +0700

    menambahkan class DetailMhsViewModel

--- app/src/main/java/com/example/pertemuan12/ui/viewmodel/DetailMhsViewModel.kt
+++ app/src/main/java/com/example/pertemuan12/ui/viewmodel/DetailMhsViewModel.kt
@@ -1,9 +1,34 @@
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.pertemuan12.model.Mahasiswa
+import com.example.pertemuan12.repository.MahasiswaRepository
 import com.example.pertemuan12.ui.viewmodel.InsertUiEvent
+import kotlinx.coroutines.launch
 
 
+class DetailMhsViewModel(private val mhsRepository: MahasiswaRepository) : ViewModel() {
 
+    var uiState by mutableStateOf(DetailUiState())
+        private set
+
+    fun fetchDetailMahasiswa(nim: String) {
+        viewModelScope.launch {
+            uiState = DetailUiState(isLoading = true)
+            try {
+                val mahasiswa = mhsRepository.getMahasiswabyNim(nim)
+                uiState = DetailUiState(detailUiEvent = mahasiswa.toDetailUiEvent())
+            } catch (e: Exception) {
+                e.printStackTrace()
+                uiState = DetailUiState(isError = true, errorMessage = "Failed to fetch details: ${e.message}")
+            }
+        }
+    }
+
+}
 
 data class DetailUiState(
     val detailUiEvent: InsertUiEvent = InsertUiEvent(),
